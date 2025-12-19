060b24487b6eb0fc2d2a80f632818d572564855e
Afu-web <23weif@gmail.com>
2025-12-19 22:54:48 +0800

    Add files via upload

---
+++ participants_data.js
@@ -0,0 +1,20 @@
+// 12月21日台中車站前毛拍 - 參加人員資料
+// 此檔案為 100% 伺服器相容方案，確保在任何託管環境皆可讀取。
+window.PARTICIPANTS_DATA = {
+    furry: [
+        { name: "曉禔", description: "主揪", fb: "https://www.facebook.com/23weif?locale=zh_TW" },
+        { name: "玄野", description: "暫無", fb: "https://www.facebook.com/xuan.ye.908297?locale=zh_TW" },
+        { name: "晴浪", description: "流量密碼", fb: "https://www.facebook.com/ChungLungSW?locale=zh_TW" },
+        { name: "緋閻月", description: "暫無", fb: "https://www.facebook.com/fei.yanyue?locale=zh_TW" }
+    ],
+    helper: [
+        { name: "許尼", role: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" },
+        { name: "沓羅", role: "暫無", fb: "https://www.facebook.com/taroOwO?locale=zh_TW" },
+        { name: "白熊", role: "網黃", fb: "https://www.facebook.com/li.yi.min.230404?locale=zh_TW" },
+        { name: "多夢", role: "遠道而來", fb: "https://www.facebook.com/Domo0312?locale=zh_TW" },
+        { name: "寒暄", role: "很會唱歌", fb: "https://www.facebook.com/han.xuan.638973?locale=zh_TW" }
+    ],
+    photographer: [
+        { name: "許尼", gear: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" }
+    ]
+};
