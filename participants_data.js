e6f6b22edab085fc41fa80bd2d48e761eae6a76c
Afu-web <23weif@gmail.com>
2025-12-19 23:00:33 +0800

    Update participants_data.js

--- participants_data.js
+++ participants_data.js
@@ -5,16 +5,19 @@ window.PARTICIPANTS_DATA = {
         { name: "曉禔", description: "主揪", fb: "https://www.facebook.com/23weif?locale=zh_TW" },
         { name: "玄野", description: "暫無", fb: "https://www.facebook.com/xuan.ye.908297?locale=zh_TW" },
         { name: "晴浪", description: "流量密碼", fb: "https://www.facebook.com/ChungLungSW?locale=zh_TW" },
-        { name: "緋閻月", description: "暫無", fb: "https://www.facebook.com/fei.yanyue?locale=zh_TW" }
+        { name: "緋閻月", description: "明星", fb: "https://www.facebook.com/fei.yanyue?locale=zh_TW" },
+        { name: "丁香", description: "暫定", fb: "https://www.facebook.com/fei.yanyue?locale=zh_TW" }
     ],
     helper: [
         { name: "許尼", role: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" },
         { name: "沓羅", role: "暫無", fb: "https://www.facebook.com/taroOwO?locale=zh_TW" },
         { name: "白熊", role: "網黃", fb: "https://www.facebook.com/li.yi.min.230404?locale=zh_TW" },
         { name: "多夢", role: "遠道而來", fb: "https://www.facebook.com/Domo0312?locale=zh_TW" },
-        { name: "寒暄", role: "很會唱歌", fb: "https://www.facebook.com/han.xuan.638973?locale=zh_TW" }
+        { name: "寒暄", role: "很會唱歌", fb: "https://www.facebook.com/han.xuan.638973?locale=zh_TW" },
+        { name: "犬冽", role: "暫無", fb: "https://www.facebook.com/lin.zheng.xing.501285?locale=zh_TW" }
     ],
     photographer: [
         { name: "許尼", gear: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" }
     ]
 };
+
