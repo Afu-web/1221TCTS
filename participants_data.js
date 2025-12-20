051a25821dabb9933f01c7893a2aa60b3e61a57c
Afu-web <23weif@gmail.com>
2025-12-20 17:16:58 +0800

    Add files via upload

--- participants_data.js
+++ participants_data.js
@@ -1,25 +1,17 @@
 // 12月21日台中車站前毛拍 - 參加人員資料
 // 此檔案為 100% 伺服器相容方案，確保在任何託管環境皆可讀取。
 window.PARTICIPANTS_DATA = {
-    furry: [
-        { name: "曉禔", description: "主揪", fb: "https://www.facebook.com/23weif?locale=zh_TW" },
-        { name: "玄野", description: "暫無", fb: "https://www.facebook.com/xuan.ye.908297?locale=zh_TW" },
-        { name: "晴浪", description: "流量密碼", fb: "https://www.facebook.com/ChungLungSW?locale=zh_TW" },
-        { name: "緋閻月", description: "明星", fb: "https://www.facebook.com/fei.yanyue?locale=zh_TW" },
-        { name: "丁香", description: "暫定", fb: "https://www.facebook.com/da.er.duo.de.diao.ding.xiang?locale=zh_TW" }
-    ],
-    helper: [
-        { name: "許尼", role: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" },
-        { name: "沓羅", role: "暫無", fb: "https://www.facebook.com/taroOwO?locale=zh_TW" },
-        { name: "白熊", role: "網黃", fb: "https://www.facebook.com/li.yi.min.230404?locale=zh_TW" },
-        { name: "多夢", role: "遠道而來", fb: "https://www.facebook.com/Domo0312?locale=zh_TW" },
-        { name: "寒暄", role: "很會唱歌", fb: "https://www.facebook.com/han.xuan.638973?locale=zh_TW" },
-        { name: "犬冽", role: "暫無", fb: "https://www.facebook.com/lin.zheng.xing.501285?locale=zh_TW" }
-    ],
-    photographer: [
-        { name: "許尼", gear: "萬能的", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" },
-        { name: "測試", gear: "這行是測試呦", fb: "" }
+    participants: [
+        { name: "曉禔(Afu)", note: "主揪", fb: "https://www.facebook.com/23weif?locale=zh_TW" },
+        { name: "白熊", note: "養胖", fb: "https://www.facebook.com/li.yi.min.230404?locale=zh_TW" },
+        { name: "丁香", note: "高歌一曲", fb: "https://www.facebook.com/da.er.duo.de.diao.ding.xiang?locale=zh_TW" },
+        { name: "許尼", note: "全能型", fb: "https://www.facebook.com/xu.ni.310702?locale=zh_TW" },
+        { name: "晴雲", note: "12月壽星", fb: "https://www.facebook.com/xia.no.hu?locale=zh_TW" },
+        { name: "沓羅", note: "樂手老師", fb: "https://www.facebook.com/taroOwO?locale=zh_TW" },
+        { name: "玄野", note: "戲腔老師", fb: "https://www.facebook.com/xuan.ye.908297?locale=zh_TW" },
+        { name: "犬冽", note: "12月壽星", fb: "https://www.facebook.com/lin.zheng.xing.501285?locale=zh_TW" },
+        { name: "星塵狼狼", note: "遠道而來", fb: "https://www.facebook.com/boris244?locale=zh_TW" },
+        { name: "晴浪(中龍大大)", note: "流量密碼", fb: "https://www.facebook.com/ChungLungSW?locale=zh_TW" },
+        { name: "寒暄", note: "歌唱老師", fb: "https://www.facebook.com/han.xuan.638973?locale=zh_TW" }
     ]
 };
-
-
